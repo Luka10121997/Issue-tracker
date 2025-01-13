395de70daca0fa3c7c1225f94d86590a712fed1b
Luka Petricevic <luka.petricevic@protestsolutions.net>
2025-01-13 22:00:59 +0100

    Created api POST action, added Issues page and Theme

---
+++ app/api/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { title } from "process";
+import { z } from 'zod';
+import { prisma } from "@/prisma/client"
+
+const createdIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1)
+})
+
+export async function POST(request: NextRequest) {
+  const body = await request.json()
+  const validation = createdIssueSchema.safeParse(body)
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  const newCreatedIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description }
+  })
+  return NextResponse.json(newCreatedIssue, { status: 201 })
+}
