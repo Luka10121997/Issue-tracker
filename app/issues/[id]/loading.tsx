5bc4fd375b7b265a426a8e33395d153df43f908f
Luka Petricevic <luka.petricevic@protestsolutions.net>
2025-03-12 10:58:05 +0100

    Styling of issue details page

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Box, Card, Flex } from '@radix-ui/themes'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div>Loading....</div>
+    <Box>
+      <Skeleton />
+      <Flex className='space-x-2 my-6'>
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className='prose' mt='-4'>
+        <Skeleton count={3} />
+      </Card>
+    </Box>
   )
 }
 
